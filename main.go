bb9034b2674c10da1c0dedc7d1a7d3dada926c6e
Huang Junlang <jlhuangch@gmail.com>
2021-07-24 23:48:03 +0800

    feat: add casdoor sdk to backend

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
+/*
+ * @Author: Junlang
+ * @Date: 2021-07-18 00:42:25
+ * @LastEditTime: 2021-07-23 17:04:50
+ * @LastEditors: Junlang
+ * @FilePath: /openscore/main.go
+ */
 package main
 
 import (
 	_ "openscore/routers"
+
+	// "github.com/astaxie/beego"
+	// "github.com/astaxie/beego/plugins/cors"
+
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	// beego.SetStaticPath("/static", "web/build/static")
+	// beego.InsertFilter("/", beego.BeforeRouter, routers.TransparentStatic) // must has this for default page
+	// beego.InsertFilter("/*", beego.BeforeRouter, routers.TransparentStatic)
+
+	beego.BConfig.WebConfig.Session.SessionName = "openscore_session_id"
+	beego.BConfig.WebConfig.Session.SessionProvider = "file"
+	beego.BConfig.WebConfig.Session.SessionProviderConfig = "./tmp"
+	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime = 3600 * 24 * 365
+
 	beego.Run()
 }
-
---
+++ util/path.go
@@ -0,0 +1,44 @@
+// Copyright 2020 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func FileExist(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func ListFiles(path string) []string {
+	res := []string{}
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, f := range files {
+		if !f.IsDir() {
+			res = append(res, f.Name())
+		}
+	}
+
+	return res
+}
