c1b1386d90f8bc508ccedb92ab3287a96500cdfb
David <jlhuangch@gmail.com>
2021-08-09 20:27:38 +0800

    feat: Problem api

--- controllers/score.go
+++ controllers/score.go
@@ -102,3 +102,85 @@ func (c *TestPaperApiController) Point() {
 	record.Save()
 }
 
+func (c *TestPaperApiController) Problem() {
+	defer c.ServeJSON()
+	var requestBody map[string]interface{}
+	json.Unmarshal(c.Ctx.Input.RequestBody, &requestBody)
+	userIdstr := requestBody["userId"].(string)
+	problemTypestr := requestBody["problemType"].(string)
+	testIdstr := requestBody["testId"].(string)
+	userId, _ := strconv.ParseInt(userIdstr, 10, 64)
+	testId, _ := strconv.ParseInt(testIdstr, 10, 64)
+	problemType, _ := strconv.ParseInt(problemTypestr, 10, 64)
+	var underTest models.UnderCorrectedPaper
+	var record models.ScoreRecord
+	var test models.TestPaper
+	test.GetTestPaper(testId)
+	test.Problem_type = problemType
+	test.Update()
+	record.Test_record_type = 5
+	record.Score_type = 1
+	record.Test_id = testId
+	record.User_id = userId
+	record.Save()
+	underTest.GetUnderCorrectedPaper(testId)
+	var newUnderTest = underTest
+	underTest.Delete()
+	newUnderTest.Test_question_type = 5
+	newUnderTest.Save()
+}
+
+func (c *TestPaperApiController) Answer() {
+	defer c.ServeJSON()
+	var requestBody map[string]interface{}
+	json.Unmarshal(c.Ctx.Input.RequestBody, &requestBody)
+	// userIdstr := requestBody["userId"].(string)
+	testIdstr := requestBody["testId"].(string)
+	testId, _ := strconv.ParseInt(testIdstr, 10, 64)
+	var test models.TestPaper
+	test.GetTestPaper(testId)
+	keyId := test.Answer_test_id
+	var as []models.TestPaperInfo
+	models.GetTestInfoListByTestId(keyId, &as)
+	data := make(map[string]interface{})
+	data["keyTest"] = as
+	resp := Response{"10000", "ok", data}
+	c.Data["json"] = resp
+}
+
+func (c *TestPaperApiController) ExampleDeatil() {
+	defer c.ServeJSON()
+	var requestBody map[string]interface{}
+	json.Unmarshal(c.Ctx.Input.RequestBody, &requestBody)
+	// userIdstr := requestBody["userId"].(string)
+	testIdstr := requestBody["exampleTestId"].(string)
+	testId, _ := strconv.ParseInt(testIdstr, 10, 64)
+	var test models.TestPaper
+	test.GetTestPaper(testId)
+	var topic models.Topic
+	topic.GetTopic(test.Question_id)
+	var tests []models.TestPaperInfo
+	models.GetTestInfoListByTestId(testId, &tests)
+	data := make(map[string]interface{})
+	data["questionName"] = topic.Question_name
+	data["test"] = tests
+	resp := Response{"10000", "ok", data}
+	c.Data["json"] = resp
+
+}
+
+func (c *TestPaperApiController) ExampleList() {
+	c.ServeJSON()
+	var requestBody map[string]interface{}
+	json.Unmarshal(c.Ctx.Input.RequestBody, &requestBody)
+	// userIdstr := requestBody["userId"].(string)
+	testIdstr := requestBody["testId"].(string)
+	testId, _ := strconv.ParseInt(testIdstr, 10, 64)
+	var testPaper models.TestPaper
+	testPaper.GetTestPaper(testId)
+	data := make(map[string]interface{})
+	data["exampleTestId"] = testPaper.Example_test_id
+	resp := Response{"10000", "ok", data}
+	c.Data["json"] = resp
+
+}
