33eca72cf94a0020dc83adcba4576e4d112ec71f
scales <a337918449@qq.com>
2021-08-14 18:24:32 +0800

    delete some code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,28 +16,3 @@ function App() {
 }
 
 export default App;
-// import './App.less';
-// import React, { Component } from 'react'
-// import {Switch,Route,Redirect,BrowserRouter as Router} from 'react-router-dom'
-
-// import Login from './views/Login'
-// import Home from './views/Home'
-// import ProjectManagement from './views/ProjectManagement'
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div id="App">
-//         <Router>
-//           <Switch>
-//             <Redirect from="/" to="/home" exact></Redirect>
-//             <Route path="/project-management" component={ProjectManagement} exact></Route>
-//             <Route path="/login" component={Login} exact></Route>
-//             <Route path="/home" component={Home}></Route>
-//           </Switch>
-//         </Router>
-//       </div>
-//     )
-//   }
-// }
-
