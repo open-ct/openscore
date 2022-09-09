dea7d571bf118a300d2014ccc329f58cfd001eac
Lruler <75160088+Lruler@users.noreply.github.com>
2022-09-09 15:25:25 +0800

    fix: fix redirect and upload warning (#52)
    * [feat]: add new Page for not lgoin
    
    * [feat]: add new Page for not lgoin
    
    * [fix]: fix menu route and get next paper after point
    
    * [fix]: fix menu route and get next paper after point
    
    * fix: fix redirect and upload warning

--- web/src/views/Manage/paper_manage/paper/index.js
+++ web/src/views/Manage/paper_manage/paper/index.js
@@ -47,10 +47,12 @@ export default class index extends Component {
         authorization: null,
       },
       onChange(info) {
-        if (info.file.status === "done") {
-          message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === "error") {
-          message.error(`${info.file.name} file upload failed.`);
+        if (info.file.response) {
+          if (info.file.response.status === "10000") {
+            message.success(`${info.file.name} file uploaded successfully`);
+          } else if (info.file.response.status === "error") {
+            message.error(`${info.file.name} file upload failed.`);
+          }
         }
       },
     };
