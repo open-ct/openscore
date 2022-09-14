f28a8a70492e118432f2e4f83cd6468dd8bcd3fa
Lruler <75160088+Lruler@users.noreply.github.com>
2022-09-14 22:59:09 +0800

    feat: Complete the docking with the backend (#56)
    * [feat]: add new Page for not lgoin
    
    * [feat]: add new Page for not lgoin
    
    * [fix]: fix menu route and get next paper after point
    
    * [fix]: fix menu route and get next paper after point
    
    * feat: Complete the docking with the backend

--- web/src/views/Train/evaluate/index.js
+++ web/src/views/Train/evaluate/index.js
@@ -1,39 +1,96 @@
 import {Select, Table} from "antd";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Manage from "../../../api/manage";
 
 import "./index.less";
 
-const columns = [
-  {
-    title: "A组",
-    dataIndex: "group",
-  },
-  {
-    title: "一致率",
-    dataIndex: "concordance",
-  },
-  {
-    title: "1",
-    dataIndex: "test_id",
-  },
-];
-const data = [];
-
-for (let i = 0; i < 46; i++) {
-  data.push({
-    key: i,
-    group: `Edward King ${i}`,
-    concordance: 32,
-    test_id: `London, Park Lane no. ${i}`,
-  });
-}
-
 const Evaluate = () => {
+
+  const [group, setGroupList] = useState([]);
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: "用户",
+      dataIndex: "account",
+    },
+    {
+      title: "一致率",
+      dataIndex: "concordance_rate",
+    },
+    {
+      title: "题目分数",
+      dataIndex: "scores",
+    },
+    {
+      title: "操作",
+      dataIndex: "",
+      render: (_, record) => <a
+        onClick={() => {
+          console.log(record);
+        }}
+        style={{width: 50}}>
+          合格
+      </a>,
+    },
+  ];
+
+  useEffect(() => {
+    Manage.getListPaperGroups().then((res) => {
+      if (res.data.data !== null) {
+        setGroupList(res.data.data.groups);
+      }
+    });
+  }, []);
+
+  const handleSelectChange = (group_id) => {
+    Manage.getListGroupGrades({group_id}).then((res) => {
+
+      if (res.data.data !== null) {
+        const {scores, teacher_grades} = res.data.data;
+        const scoreStr = scores.join("-");
+        const newData = [];
+        newData.push({
+          scores: scoreStr,
+          account: "管理员",
+          concordance_rate: "/",
+        });
+        if (teacher_grades !== null) {
+          teacher_grades.map((teacher) => {
+            newData.push({
+              key: teacher.teacher_account,
+              account: teacher.teacher_account,
+              concordance_rate: teacher.concordance_rate,
+              scores: teacher.scores.join("-"),
+            });
+          });
+        } else {
+          newData.push(
+            {
+              account: "暂未有人完成评卷",
+            }
+          );
+        }
+        setData(newData);
+      }
+
+    });
+  };
+
+  // for (let i = 0; i < 46; i++) {
+  //   data.push({
+  //     key: i,
+  //     group: `Edward King ${i}`,
+  //     concordance: 32,
+  //     test_id: `London, Park Lane no. ${i}`,
+  //   });
+  // }
 
   return (
     <div className="evaluate-page">
       <div className="search-container">
-        <Select placeholder="组别选择"></Select >
+        <Select placeholder="组别选择" onChange={handleSelectChange}>
+          {group.map((g) => <Select.Option key={g.group_id} value={g.group_id}>{g.group_name}</Select.Option>)}
+        </Select >
       </div>
 
       <div className="display-container">
